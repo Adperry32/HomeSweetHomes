87f0fdc2025df3c5d220e7943d1dbd1af47f8407
Arrington Perry <perryceo21@hotmail.com>
2023-03-02 20:32:30 -0500

    Fixed screens and added Account Fragment

---
+++ src/main/java/com/example/homesweethomes2/RegShortFragment.kt
@@ -0,0 +1,26 @@
+package com.example.homesweethomes2
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class RegShortFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view =  inflater.inflate(R.layout.fragment_reg_short, container, false)
+
+
+        return view
+    }
+
+
+
+}
